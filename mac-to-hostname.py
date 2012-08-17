83f76c2125ff8cdc00c4bbcb8aca33ffae69792f
Mark Christian <markdotchristian@yahoo.com>
2012-08-17 02:30:13 -0700

    just learning

--- mac-to-hostname.py
+++ mac-to-hostname.py
@@ -7,26 +7,15 @@
 COBBLER_MASTER="137.57.206.151"
 
 import xmlrpclib
-import fcntl, socket, struct
 import subprocess
-#from subprocess import call
 
 server = xmlrpclib.Server("http://" + COBBLER_MASTER + "/cobbler_api")
 
-#def getHwAddr(ifname): 
-#  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#  info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
-#  return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
-
-#MAC =  getHwAddr('eth0')
-# Or skip the def and just do:
 MAC =  subprocess.Popen("/sbin/ifconfig eth0|/bin/awk '/HWaddr/ {print $5}'", stdout=subprocess.PIPE, shell=True).stdout.read()
 
 SYSTEM = server.find_system({"mac":MAC})
 
-URL = "http://%s/cblr/svc/op/nopxe/system/%s" %  (COBBLER_MASTER , SYSTEM[0]) 
-# below does not work, why?
-# why can't i pass "-O"?: URL = "http://%s/cblr/svc/op/nopxe/system/%s" %  (COBBLER_MASTER , SYSTEM[0]) + " -O /dev/null"
+URL = "http://%s/cblr/svc/op/nopxe/system/%s" %  (COBBLER_MASTER , SYSTEM[0])  + " -O /dev/null"
 
 subprocess.call(["/usr/bin/wget", URL])
 
